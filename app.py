bdb3afdb3492e106879caf58c5c3c5578a4faf4f
Amy <92268808+AmyVerse@users.noreply.github.com>
2025-09-24 10:23:09 +0530

    prod ready

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from fastapi.templating import Jinja2Templates
 import uvicorn
 import pickle
 import pandas as pd
+import os
 
 app = FastAPI()
 
@@ -93,4 +94,5 @@ def predict(
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
